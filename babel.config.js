e2e16c414bbeae11af574dedf572a34895aa07d7
Florent MARQUES <63flotoof@gmail.com>
2023-03-07 10:27:14 +0100

    🔧 config ts module resolution

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: ['.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@src': './src',
+          '@components': './src/components',
+          '@store': './src/store',
+        },
+      },
+    ],
+  ],
 };
